7ed9d09d6ec7686be1c76fb92e708fb64a7a9b37
moises <00400815@uca.edu.sv>
2023-04-07 23:51:21 -0600

    feat: exercise 2

---
+++ 2.vars-objs/intro.js
@@ -0,0 +1,11 @@
+const me = {
+    name: 'John',
+    age: 30,
+    developer: true,
+    fav_book: {
+        title: 'The Alchemist',
+        author: 'Paulo Coelho',
+        year: 1988,
+        url: 'https://en.wikipedia.org/wiki/The_Alchemist_(novel)'
+    }
+}
